354643a083a028a5acabce668ca70b70f3fc574e
gayathri8809 <gayathri8809@gmail.com>
2019-01-24 13:27:25 -0500

    Added comments in arimetic_math class

---
+++ src/practice_Java/arimetic_math.java
@@ -0,0 +1,23 @@
+package practice_Java;
+
+public class arimetic_math {
+
+	public static void main(String[] args) {
+		
+		int i=100,j=20;
+		//adding 
+		int Add = i+j+j+j+j;
+		//Subtracting
+		int Sub = i-j;
+		//Multiplying
+		int Mul = i*j;
+		//dividing
+		int Div = i/j;
+		
+		System.out.println("The results are " +Add +"," +Sub+"," +Mul+"," +Div);
+		
+				// TODO Auto-generated method stub
+
+	}
+
+}
